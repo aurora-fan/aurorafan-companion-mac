7baecba0d1590c2310d53c9eafc4e73d3df069a6
Michael Pavkovic <mpavkovic688@gmail.com>
2019-12-13 11:51:42 -0500

    Initial Commit

---
+++ aurorafan-companion-mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  aurorafan-companion-mac
+//
+//  Created by Michael Pavkovic on 12/13/19.
+//  Copyright © 2019 AuroraFan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ aurorafan-companion-mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  aurorafan-companion-mac
+//
+//  Created by Michael Pavkovic on 12/13/19.
+//  Copyright © 2019 AuroraFan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
